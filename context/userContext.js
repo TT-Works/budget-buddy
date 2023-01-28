218dd0414b3142ac4c0ec7fd6546a6b32865d75a
Tsvetislav Todorov <tsvetislav.todorov@MBGNM0107.local>
2023-01-28 11:17:16 +0200

    Init

---
+++ context/userContext.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const UserContext = React.createContext(null);
+
+export const UserProvider = ({ children }) => {
+  const useIsomorphicLayoutEffect =
+    typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
+  const [user, setUser] = React.useState('');
+
+  useIsomorphicLayoutEffect(() => {
+    const user = window.localStorage.getItem('user');
+    if (user) {
+      setUser(user);
+    }
+  }, []);
+
+  const value = React.useMemo(() => [user, setUser], [user]);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
